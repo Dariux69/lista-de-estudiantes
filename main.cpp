ff794fc494cb93216b67db825d7ce2c6dbf29ab7
Bran-ssj <00109425@uca.edu.sv>
2026-01-16 17:13:49 -0600

    add: clase estudiante

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
+
+//clase estudiante
+class Estudiante {
+private:
+    string nombre;
+    float notaFinal;
+
+public:
+    Estudiante(string nombre, float notaFinal) {
+        this->nombre = nombre;
+        this->notaFinal = notaFinal;
+    }
+
+    float tomarNota() const {
+        return notaFinal;
+    }
+
+    void imprimir() const {
+        cout << "Nombre: " << nombre
+             << "  Nota final: " << notaFinal << endl;
+    }
+};
 
 int main() {
     return 0;
